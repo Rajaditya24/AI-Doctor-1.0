7b2989bc9451a551ff253a8baf4d7aaf6f0b829b
Rajaditya24 <adityarajjadhav05@gmail.com>
2025-11-11 20:46:35 +0530

    Delete medbot_1 directory

--- medbot_1/utils.py
+++
@@ -1,6 +0,0 @@
-# Utility functions for medbot
-
-def extract_symptoms(text):
-    # Placeholder for advanced symptom extraction logic.
-    # This function is not currently used in the application.
-    return []
